9435f4697ba118a32513991c4a1aa2b01c530729
Pepe Roldán <jmroldangil@gmail.com>
2021-03-02 18:31:03 +0100

    Setting proper version

--- src/main/java/com/learning/game/GameApplication.java
+++ src/main/java/com/learning/game/GameApplication.java
@@ -6,11 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameApplication {
 
-  //    @Bean
-  //    public GameService gameDal() {
-  //        return new GameService();
-  //    }
-
   public static void main(String[] args) {
     SpringApplication.run(GameApplication.class, args);
   }
